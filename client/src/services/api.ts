fa22148ffc3cfc86b8cc231c6739f8395e351a53
neerajg-03 <gupta.neeraj9000@gmail.com>
2026-04-19 16:24:57 +0530

    Update api.ts

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -15,9 +15,12 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
+
     if (token) {
+      config.headers = config.headers || {};
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
   (error) => Promise.reject(error)
